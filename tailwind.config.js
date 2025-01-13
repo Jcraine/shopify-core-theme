2e15f6402b37dc6b1d0a192015ab5af636b4ebc5
Jason Che <jason@teamgeek.io>
2025-01-12 17:10:14 -0800

    feature: product detail page swatch component

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./**/*.{liquid,json}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: '#FF6633',
+        green: '#006600',
+        blue: '#00639C',
+        yellow: '#FCE78D',
+        pink: '#FFCCFF',
+        navy: '#19264B',
+      },
+    },
   },
   plugins: [],
 };

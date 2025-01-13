315d336c9b75971210d1f6a08c6a4e165566a7de
Jason Che <jason@teamgeek.io>
2025-01-13 10:35:24 -0800

    feature: update product card to include brand and use tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        'sale-orange': '#FF0000',
         orange: '#FF6633',
         green: '#006600',
         blue: '#00639C',
         yellow: '#FCE78D',
         pink: '#FFCCFF',
         navy: '#19264B',
+        grey: '#E8E8E8',
       },
     },
   },
